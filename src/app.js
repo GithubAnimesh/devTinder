578710701d26fd1e14467b39df2c65644f2567ee
Animesh Singh <animesh.singh@gmail.com>
2025-11-26 21:01:48 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -4,12 +4,26 @@ const app = express(); // create new appliction of express.
 
 // handle code, this is called request handler
 
-app.get("/user", (req, res) => {
-  res.send({ name: "Animesh", lastName: "Sinngh" });
-});
+// app.get("/user", (req, res) => {
+//   res.send({ name: "Animesh", lastName: "Sinngh" });
+// });
 
-app.post("/user", (req, res) => {
-  res.send("Date save successfully");
+// app.post("/user", (req, res) => {
+//   res.send("Date save successfully");
+// });
+
+app.get("/getUserData", (req, res) => {
+  try {
+    throw new Error("dfsdbf");
+    res.send("User data send");
+  } catch (err) {
+    res.status(500).send(`Something went wrong, ${err.message}`);
+  }
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong user ");
+  }
 });
 
 app.listen(3000, () => {
